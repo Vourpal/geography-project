860811ea010ceea5a179825ebf7337f409990073
Bruno Oros <hollowxdstorm@gmail.com>
2023-02-06 19:58:31 -0500

    intial commit

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import { obtainAllCountries } from "./api";
 const Country = () => {
- 
+  const [block, setBlock] = useState([]);
+  const [filteredBlock, setFilteredBlock] = useState([]);
+  const [searchedBlock, setSearchedBlock] = useState([]);
+  const allCountries = () => {
+    setBlock(
+      obtainAllCountries().then((res) => {
+        return res.data.map((e) => {
+          return e.name
+          
+        })
+      })
+    );
+  };
+
   return (
     <div>
+      <div>{block && <p>{block}</p>}</div>
       <div>image will be placed here</div>
       <div>countryName</div>
       <div>countryPopulation</div>
       <div>countryRegion</div>
       <div>countryCapital </div>
 
-      <button>click me</button>
-      <button>click me after</button>
+      <button onClick={allCountries}>click me</button>
+      <button onClick={() => {
+        console.log(block)
+      }}>click me after</button>
     </div>
   );
 };
